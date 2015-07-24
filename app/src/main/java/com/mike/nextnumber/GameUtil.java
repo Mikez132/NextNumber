949f02e98bba7a5e48cb3c21969549e358cf5bf1
Mikez132 <metalmike323@gmail.com>
2015-07-23 17:01:40 -0700

    Started Util Class

---
+++ app/src/main/java/com/mike/nextnumber/GameUtil.java
@@ -0,0 +1,36 @@
+package com.mike.nextnumber;
+
+/**
+ * Created by Mike on 7/23/2015.
+ * conatins satis methods that the main game will use
+ */
+
+import java.util.Random;
+public class GameUtil {
+
+
+   /*
+   *generates a radnom numb up to n
+   @param n - Upper Bound
+   @return- Random Number
+    */
+    public static int genenratenewNumb(int n) {
+        Random r = new Random();
+        int ranNumb = r.nextInt(n+1);
+        return ranNumb;
+    }
+    public static boolean isPrime(int n) {
+
+       //placeholder
+        return true;
+
+
+
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/mike/nextnumber/Profile.java
@@ -0,0 +1,14 @@
+package com.mike.nextnumber;
+
+import java.io.Serializable;
+
+/**
+ * Created by Mike on 7/23/2015.
+ * includes all info pertaining to player data
+ */
+public class Profile implements Serializable {
+
+
+
+
+}
